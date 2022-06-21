50a695ec4465a9325753d2ee61f1b92219ab08fa
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-06-21 16:38:25 +0200

    Ajout de la page login

--- StudiScrumApp062022/Controllers/HomeController.cs
+++ StudiScrumApp062022/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace StudiScrumApp062022.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
