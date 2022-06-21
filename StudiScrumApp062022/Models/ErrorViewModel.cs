c391c9e0418c8e1e7723ac874bca7ddd5d5af572
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-06-21 16:14:45 +0200

    Ajoutez des fichiers projet.

---
+++ StudiScrumApp062022/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudiScrumApp062022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
